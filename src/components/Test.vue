8d78d6dc1def1a26dfbc32a87aac4fd9f096b904
崔玥 <cuiyue@cuiyuedeMBP.lan>
2020-10-02 10:21:18 +0800

    空项目搭建

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>test component:{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "abc",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
